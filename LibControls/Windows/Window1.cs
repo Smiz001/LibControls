ed47b5264216ffbdf48b8a6392d9016eee1994e3
PosNik <kenismiz@gmail.com>
2019-05-17 10:38:53 +0300

    Add project files.

---
+++ LibControls/Windows/Window1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Shapes;
+
+namespace LibControls.Windows
+{
+  public class Window1:Window
+  {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+    private HwndSource _hwndSource;
+
+    string title;
+    //public Window1(string title) : base()
+    //{
+    //  PreviewMouseMove += OnPreviewMouseMove;
+    //  WindowStartupLocation = WindowStartupLocation.CenterScreen;
+    //  this.title = title;
+    //}
+
+    static Window1()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(Window1),
+          new FrameworkPropertyMetadata(typeof(Window1)));
+    }
+
+    #region Click events
+
+    protected void MinimizeClick(object sender, RoutedEventArgs e)
+    {
+      WindowState = WindowState.Minimized;
+    }
+
+    protected void RestoreClick(object sender, RoutedEventArgs e)
+    {
+      WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+    }
+
+    protected void CloseClick(object sender, RoutedEventArgs e)
+    {
+      Close();
+    }
+
+    private void MouseLeftButtonDownOntopGrid(object sender, MouseButtonEventArgs e)
+    {
+      //// base.OnMouseLeftButtonDown(e);
+      if (Mouse.LeftButton == MouseButtonState.Pressed)
+        this.DragMove();
+    }
+    #endregion
+
+    public override void OnApplyTemplate()
+    {
+      Button minimizeButton = GetTemplateChild("minimizeButton") as Button;
+      if (minimizeButton != null)
+      {
+        minimizeButton.Click += MinimizeClick;
+        if (ResizeMode == ResizeMode.NoResize)
+          minimizeButton.Visibility = Visibility.Hidden;
+      }
+
+      Button restoreButton = GetTemplateChild("restoreButton") as Button;
+      if (restoreButton != null)
+      {
+        restoreButton.Click += RestoreClick;
+        if (ResizeMode == ResizeMode.NoResize)
+          restoreButton.Visibility = Visibility.Hidden;
+      }
+
+      Button closeButton = GetTemplateChild("closeButton") as Button;
+      if (closeButton != null)
+        closeButton.Click += CloseClick;
+
+      TextBlock tblTitle = GetTemplateChild("tblTitle") as TextBlock;
+      if (tblTitle != null)
+        tblTitle.Text += title;
+
+
+      Grid gridBtn = GetTemplateChild("gridForBtn") as Grid;
+      if (gridBtn != null)
+        gridBtn.MouseLeftButtonDown += MouseLeftButtonDownOntopGrid;
+
+      if (ResizeMode != ResizeMode.NoResize)
+      {
+
+        Grid resizeGrid = GetTemplateChild("resizeGrid") as Grid;
+        if (resizeGrid != null)
+        {
+          foreach (UIElement element in resizeGrid.Children)
+          {
+            Rectangle resizeRectangle = element as Rectangle;
+            if (resizeRectangle != null)
+            {
+              resizeRectangle.PreviewMouseDown += ResizeRectangle_PreviewMouseDown;
+              resizeRectangle.MouseMove += ResizeRectangle_MouseMove;
+            }
+          }
+        }
+      }
+
+      base.OnApplyTemplate();
+    }
+
+    protected void ResizeRectangle_MouseMove(Object sender, MouseEventArgs e)
+    {
+      Rectangle rectangle = sender as Rectangle;
+      switch (rectangle.Name)
+      {
+        case "top":
+          Cursor = Cursors.SizeNS;
+          break;
+        case "bottom":
+          Cursor = Cursors.SizeNS;
+          break;
+        case "left":
+          Cursor = Cursors.SizeWE;
+          break;
+        case "right":
+          Cursor = Cursors.SizeWE;
+          break;
+        case "topLeft":
+          Cursor = Cursors.SizeNWSE;
+          break;
+        case "topRight":
+          Cursor = Cursors.SizeNESW;
+          break;
+        case "bottomLeft":
+          Cursor = Cursors.SizeNESW;
+          break;
+        case "bottomRight":
+          Cursor = Cursors.SizeNWSE;
+          break;
+        default:
+          break;
+      }
+    }
+
+    //private void moveRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    //{
+    //  if (Mouse.LeftButton == MouseButtonState.Pressed)
+    //    DragMove();
+    //}
+
+    protected void OnPreviewMouseMove(object sender, MouseEventArgs e)
+    {
+      if (Mouse.LeftButton != MouseButtonState.Pressed)
+        Cursor = Cursors.Arrow;
+    }
+
+
+    protected void ResizeRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+      Rectangle rectangle = sender as Rectangle;
+      switch (rectangle.Name)
+      {
+        case "top":
+          Cursor = Cursors.SizeNS;
+          ResizeWindow(ResizeDirection.Top);
+          break;
+        case "bottom":
+          Cursor = Cursors.SizeNS;
+          ResizeWindow(ResizeDirection.Bottom);
+          break;
+        case "left":
+          Cursor = Cursors.SizeWE;
+          ResizeWindow(ResizeDirection.Left);
+          break;
+        case "right":
+          Cursor = Cursors.SizeWE;
+          ResizeWindow(ResizeDirection.Right);
+          break;
+        case "topLeft":
+          Cursor = Cursors.SizeNWSE;
+          ResizeWindow(ResizeDirection.TopLeft);
+          break;
+        case "topRight":
+          Cursor = Cursors.SizeNESW;
+          ResizeWindow(ResizeDirection.TopRight);
+          break;
+        case "bottomLeft":
+          Cursor = Cursors.SizeNESW;
+          ResizeWindow(ResizeDirection.BottomLeft);
+          break;
+        case "bottomRight":
+          Cursor = Cursors.SizeNWSE;
+          ResizeWindow(ResizeDirection.BottomRight);
+          break;
+        default:
+          break;
+      }
+    }
+
+    private void ResizeWindow(ResizeDirection direction)
+    {
+      SendMessage(_hwndSource.Handle, 0x112, (IntPtr)(61440 + direction), IntPtr.Zero);
+    }
+
+    private enum ResizeDirection
+    {
+      Left = 1,
+      Right = 2,
+      Top = 3,
+      TopLeft = 4,
+      TopRight = 5,
+      Bottom = 6,
+      BottomLeft = 7,
+      BottomRight = 8,
+    }
+
+    protected override void OnInitialized(EventArgs e)
+    {
+      SourceInitialized += OnSourceInitialized;
+      base.OnInitialized(e);
+    }
+
+    private void OnSourceInitialized(object sender, EventArgs e)
+    {
+      _hwndSource = (HwndSource)PresentationSource.FromVisual(this);
+    }
+  }
+}
---
+++ LibControls/Windows/Window2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LibControls.Windows
+{
+  public class Window2:Window
+  {
+    [DllImport("user32.dll")]
+    static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+    [DllImport("user32.dll")]
+    static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+    [DllImport("user32.dll")]
+    static extern int TrackPopupMenu(IntPtr hMenu, uint uFlags, int x, int y, int nReserved, IntPtr hWnd, IntPtr prcRect);
+
+    private Point startPos;
+    private System.Windows.Forms.Screen[] screens = System.Windows.Forms.Screen.AllScreens;
+
+    static Window2()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(Window2),
+          new FrameworkPropertyMetadata(typeof(Window2)));
+    }
+
+    private void Window_LocationChanged(object sender, EventArgs e)
+    {
+      int sum = 0;
+      foreach (var item in screens)
+      {
+        sum += item.WorkingArea.Width;
+        if (sum >= this.Left + this.Width / 2)
+        {
+          this.MaxHeight = item.WorkingArea.Height;
+          break;
+        }
+      }
+    }
+
+    private void System_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      if (e.ChangedButton == MouseButton.Left)
+      {
+        if (e.ClickCount >= 2)
+        {
+          this.WindowState = (this.WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+        else
+        {
+          startPos = e.GetPosition(null);
+        }
+      }
+      else if (e.ChangedButton == MouseButton.Right)
+      {
+        var pos = PointToScreen(e.GetPosition(this));
+        IntPtr hWnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+        IntPtr hMenu = GetSystemMenu(hWnd, false);
+        int cmd = TrackPopupMenu(hMenu, 0x100, (int)pos.X, (int)pos.Y, 0, hWnd, IntPtr.Zero);
+        if (cmd > 0) SendMessage(hWnd, 0x112, (IntPtr)cmd, IntPtr.Zero);
+      }
+    }
+
+    private void System_MouseMove(object sender, MouseEventArgs e)
+    {
+      if (e.LeftButton == MouseButtonState.Pressed)
+      {
+        if (this.WindowState == WindowState.Maximized && Math.Abs(startPos.Y - e.GetPosition(null).Y) > 2)
+        {
+          var point = PointToScreen(e.GetPosition(null));
+
+          this.WindowState = WindowState.Normal;
+
+          this.Left = point.X - this.ActualWidth / 2;
+          this.Top = point.Y - border.ActualHeight / 2;
+        }
+        DragMove();
+      }
+    }
+
+    private void Maximize_Click(object sender, RoutedEventArgs e)
+    {
+      this.WindowState = (this.WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void Mimimize_Click(object sender, RoutedEventArgs e)
+    {
+      this.WindowState = WindowState.Minimized;
+    }
+
+    private void Window_StateChanged(object sender, EventArgs e)
+    {
+      if (this.WindowState == WindowState.Maximized)
+      {
+        main.BorderThickness = new Thickness(0);
+        main.Margin = new Thickness(7, 7, 7, 0);
+        rectMax.Visibility = Visibility.Hidden;
+        rectMin.Visibility = Visibility.Visible;
+      }
+      else
+      {
+        main.BorderThickness = new Thickness(1);
+        main.Margin = new Thickness(0);
+        rectMax.Visibility = Visibility.Visible;
+        rectMin.Visibility = Visibility.Hidden;
+      }
+    }
+  }
+}
